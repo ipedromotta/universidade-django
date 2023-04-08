dfa1579fa5a24674b5a839a4ee6d2fa23d65044d
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2023-04-08 17:06:26 -0300

    :x: Excluindo codigo antigo

--- meu_site/social/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Link
-
-@admin.register(Link)
-class LinkAdmin(admin.ModelAdmin):
-    readonly_fields = ('criado','alterado')
-    list_display = ('chave','criado','alterado') 
