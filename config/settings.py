b05312da1348b6755767da9b08a6a4f24218dbb5
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2023-04-10 19:07:48 -0300

    :lipstick: feat: Add crispy forms

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'paginas',
     'cadastros',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+# Crispy Forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
