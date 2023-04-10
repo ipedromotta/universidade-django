4f62829b5ce2e6b289e8eca5bb186c92fe0289c1
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2023-04-10 16:30:21 -0300

    :recycle: refactor: Colocando link no padrao

--- paginas/urls.py
+++ paginas/urls.py
@@ -4,5 +4,5 @@ from .views import PaginaInicial, SobreView
 
 urlpatterns = [
     path('', PaginaInicial.as_view(), name='inicio'),
-    path('/sobre', SobreView.as_view(), name='sobre'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
 ]
